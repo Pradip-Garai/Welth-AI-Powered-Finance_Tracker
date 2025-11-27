ea89914bc292515a690059a9dcceee41021c7fb6
Pradip-Garai <pradipgarai173@gmail.com>
2025-11-27 13:33:21 +0530

    Landing Page Created

--- app/page.jsx
+++ app/page.jsx
@@ -55,6 +55,24 @@ export default function Home() {
            </div>
         </section>
 
+        {/* Testimonials */}
+         <section className="py-20">
+           <div className="container mx-auto px-4">
+             <h2 className="text-3xl font-bold text-center mb-12">Everything you need to manage your finances</h2>
+             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
+               {featuresData.map((feture,index)=>(
+                 <Card key={index} className={"p-6"}>
+                   <CardContent className={"space-y-4 pt-4"}>
+                     {feture.icon}
+                     <h3 className="text-xl font-semibold">{feture.title}</h3>
+                     <p className="text-gray-600">{feture.description}</p>
+                  </CardContent>
+                </Card>
+               ))}
+             </div>
+           </div>
+        </section>
+
      </div>
   );
 }
